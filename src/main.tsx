a0be81612403d5c8f226ee3cb7cb413988892ced
Raki <killer.vp.mic@gmail.com>
2023-02-22 18:07:41 -0300

    added routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
 import { registerSW } from 'virtual:pwa-register';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from './screens/HomeScreen';
+import SavedRecipesScreen from './screens/SavedRecipesScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 const updateSW = registerSW({
   onOfflineReady() {
     // Render a dialog or a modal to show the user offline mode is ready
     alert('Offline is ready!');
   },
-})
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeScreen />,
+  },
+  {
+    path: '/savedrecipes',
+    element: <SavedRecipesScreen />,
+  },
+  {
+    path: '/settings',
+    element: <SettingsScreen />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <MantineProvider withGlobalStyles withNormalizeCSS>
-        <App />
+        <RouterProvider router={router} />
       </MantineProvider>
     </Provider>
   </React.StrictMode>,
