ed2c73f4a46577a179604656fc483daae29b177d
Raki <killer.vp.mic@gmail.com>
2023-02-23 16:22:40 -0300

    added request processor to get the data with regex

--- server/src/promptPreProcess.ts
+++ server/src/promptPreProcess.ts
@@ -1,9 +1,11 @@
-const promptPreProcess = (ingredients: string[]) => {
+const promptPreProcess = (ingredients : string[]) => {
     return `
     I'm going to give you a list of ingredients and you have to give me a recipe, its name, approximate cook time and required ingredients. If the ingredients list has an ingredient that shouldn't be used to cook or doesn't make sense, write "UNSAFE" after the "Recipe sense:". 
 
     Ingredient list:
-    -${ingredients.join(`\n-`)}
+    -${
+        ingredients.join(`\n-`)
+    }
     
     Recipe sense: 
     Recipe name: 
@@ -13,4 +15,39 @@ const promptPreProcess = (ingredients: string[]) => {
     `;
 }
 
+interface RecipeData {
+    recipeName: string; 
+    cookTime: string; 
+    ingredients: string[];
+    recipe: string;
+}
+
+const extractRecipeDataFromResponse = (response: string): RecipeData | null => {
+    const pattern: RegExp = /Recipe name:\s*(.+)\nApproximate cook time:\s*(\d+.*?)\nRequired ingredients:\s*(.*?)\nRecipe instructions:\s*(.*)/s;
+    const match: RegExpExecArray | null = pattern.exec(response);
+
+    if (match) {
+        const recipeName: string = match[1];
+        const cookTime: string = match[2];
+        const ingredients: string[] = match[3].split(', ');
+        const recipe: string = match[4];
+
+        console.log(`Recipe name: ${recipeName}`);
+        console.log(`Cook time: ${cookTime}`);
+        console.log(`Ingredients: ${ingredients}`);
+        console.log(`Recipe: ${recipe}`);
+
+        const recipeDataObject: RecipeData = {
+            recipeName,
+            cookTime,
+            ingredients,
+            recipe
+        }
+        return recipeDataObject;
+    } else {
+        return null;
+    }
+}
+
+export { extractRecipeDataFromResponse };
 export default promptPreProcess;
