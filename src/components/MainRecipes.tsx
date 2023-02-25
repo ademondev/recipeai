e5bae13e540584dd3d1a67d92ee9c509a665495a
Raki <killer.vp.mic@gmail.com>
2023-02-25 15:51:37 -0300

    added useEffect to update new images on load

--- src/components/MainRecipes.tsx
+++ src/components/MainRecipes.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { Text } from '@mantine/core';
 import RecipeComponent from './RecipeComponent';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { fetchGoogleImages } from '../features/counter/recipe/recipeDataSlice';
 
 interface MainRecipeProps {
 
 }
 
 const MainRecipe: React.FunctionComponent<MainRecipeProps> = () => {
-    const { recipeName, cookTime, ingredients, recipe } = useAppSelector(state => state.recipeData.recipeData);
+    const { recipeName, cookTime, ingredients, recipe } = useAppSelector((state) => state.recipeData.recipeData);
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        if (recipeName !== '') {
+            dispatch(fetchGoogleImages(recipeName));
+        }
+    }, [recipeName]);
     return (<>
         <Text
             weight={600}
