af78814a1979ae8d45b0915ca1559ae57ca33b9d
Raki <killer.vp.mic@gmail.com>
2023-02-26 17:42:30 -0300

    added redux-persist to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import counterReducer from '../features/counter/counterSlice.js';
 import ingredientsSlice from '../features/counter/ingredients/ingredientsSlice';
 import recipeDataSlice from '../features/counter/recipe/recipeDataSlice.js';
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedRecipeDataSlice = persistReducer(persistConfig, recipeDataSlice);
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     ingredients: ingredientsSlice,
-    recipeData: recipeDataSlice
+    recipeData: persistedRecipeDataSlice
   }
 });
+const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export { persistor };
 export default store;
