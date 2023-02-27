101ac7f52ffa6350cbf8e2afd498379cf8a59e5a
Raki <killer.vp.mic@gmail.com>
2023-02-27 16:13:16 -0300

    added searchBar filtered search using searchBarSlice

--- src/screens/SavedRecipesScreen.tsx
+++ src/screens/SavedRecipesScreen.tsx
@@ -1,14 +1,21 @@
-import { Container, createStyles, Space } from '@mantine/core';
+import { Container, Space } from '@mantine/core';
 import { useAppSelector } from '../app/hooks';
 import MainBottomMenu from '../components/MainBottomMenu';
 import SavedRecipeComponent from '../components/SavedRecipeComponent';
+import SearchBar from '../components/SearchBar';
 
 
 const SavedRecipesScreen: React.FunctionComponent = () => {
     const savedRecipes = useAppSelector(state => state.recipeData.savedRecipesStorage);
+    const searchBarState = useAppSelector(state => state.searchBar.searchBarState);
+    // recipe.recipeName must be lower case as the searchbar wouldn't be able to filter
+    // recipes properly due to the recipes having uppercase letters, not matching 
+    // unless you write the names perfectly.
+    const filteredSavedRecipes = savedRecipes.filter(recipe => recipe.recipeName.toLowerCase().includes(searchBarState));
     return (<>
+        <SearchBar />
         <Container>
-            {savedRecipes.map(recipe => {
+            {filteredSavedRecipes.map(recipe => {
                 return (
                     <SavedRecipeComponent
                         recipeName={recipe.recipeName}
