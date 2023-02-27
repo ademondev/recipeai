ddc0a02702430e57468e32db4cf4679b3a514552
Raki <killer.vp.mic@gmail.com>
2023-02-27 14:11:56 -0300

    updated SavedRecipesScreen with its components

--- src/screens/SavedRecipesScreen.tsx
+++ src/screens/SavedRecipesScreen.tsx
@@ -1,7 +1,30 @@
+import { Container, createStyles, Space } from '@mantine/core';
+import { useAppSelector } from '../app/hooks';
 import MainBottomMenu from '../components/MainBottomMenu';
+import SavedRecipeComponent from '../components/SavedRecipeComponent';
 
-function SavedRecipesScreen() {
+
+const SavedRecipesScreen: React.FunctionComponent = () => {
+    const savedRecipes = useAppSelector(state => state.recipeData.savedRecipesStorage);
     return (<>
+        <Container>
+            {savedRecipes.map(recipe => {
+                return (
+                    <SavedRecipeComponent
+                        recipeName={recipe.recipeName}
+                        cookTime={recipe.cookTime}
+                        ingredients={recipe.ingredients}
+                        recipe={recipe.recipe}
+                        src={recipe.image?.url}
+                        id={recipe.id}
+                        key={recipe.id}
+                    />
+                );
+            })}
+        </Container>
+        <Space h='xl' />
+        <Space h='xl' />
+        <Space h='xl' />
         <MainBottomMenu />
     </>);
 }
