375a650b90b65728d671954e873034fa5aa4bde1
Raki <killer.vp.mic@gmail.com>
2023-02-26 21:31:10 -0300

    changed slices' directory

---
+++ src/features/ingredients/ingredientsSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface IngredientsInterface {
+    id: number
+    name: string
+}
+
+interface InitialStateInterface {
+    ingredients: IngredientsInterface[]
+}
+
+const initialState: InitialStateInterface = {
+    ingredients: []
+}
+
+export const ingredientsSlice = createSlice({
+    name: 'ingredients',
+    initialState,
+    reducers: {
+        addIngredient: (state, action: PayloadAction<{ id: number, name: string }>) => {
+            if (action.payload.name === '') return;
+            state.ingredients.push({
+                id: action.payload.id,
+                name: action.payload.name
+            })
+        },
+        removeIngredient: (state, action: PayloadAction<number>) => {
+            state.ingredients = state.ingredients.filter(ingredient => ingredient.id !== action.payload);
+        }
+    }
+})
+
+// Action creators are generated for each case reducer function
+export const { addIngredient, removeIngredient } = ingredientsSlice.actions;
+
+export default ingredientsSlice.reducer;
---
+++ src/features/recipe/recipeDataSlice.ts
@@ -0,0 +1,158 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { Image } from 'google-images';
+
+export interface RecipeInterface {
+    recipeName: string,
+    cookTime: string,
+    ingredients: string[],
+    recipe: string[],
+    id: string;
+}
+
+enum RecipeDataStatuses {
+    PENDING = 'pending',
+    FULFILLED = 'fulfilled',
+    REJECTED = 'rejected'
+}
+
+enum GoogleImagesStatuses {
+    PENDING = 'pending',
+    FULFILLED = 'fulfilled',
+    REJECTED = 'rejected'
+}
+const DEFAULT_IMAGE_URL = "https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80";
+
+const initialState = {
+    recipeData: {
+        recipeName: '',
+        cookTime: '',
+        ingredients: [],
+        recipe: [],
+        id: ''
+    } as RecipeInterface,
+    recipeDataStatus: RecipeDataStatuses.PENDING,
+    // The default image for recipes is DEFAULT_IMAGE_URL.
+    // It will then be replaced by an array of ten images
+    // (represented by ten objects). This array should never
+    // be empty, instead, it should make another call to
+    // fetchGoogleImages before running out of images.
+    googleImagesData: [
+        {
+            url: DEFAULT_IMAGE_URL,
+            type: '',
+            height: 292,
+            width: 390,
+            size: 100,
+            thumbnail: {
+                height: 10,
+                width: 8,
+                url: ''
+            }
+        }
+    ],
+    googleImagesStatus: GoogleImagesStatuses.PENDING,
+    savedRecipes: [
+        {
+            recipeName: 'tomato',
+            cookTime: '10',
+            id: '12412sfaw',
+            ingredients: ['tomato'],
+            recipe: ['asfasfsafs']
+        }
+    ] as RecipeInterface[]
+};
+
+const ENDPOINT_RECIPE_DATA = 'http://localhost:5000/completion';
+const ENDPOINT_GOOGLE_IMAGES = 'http://localhost:5000/images';
+
+const fetchRecipeData = createAsyncThunk('recipeData/fetchRecipeData', async (ingredientsArray : string[]) => {
+    const response = await fetch(ENDPOINT_RECIPE_DATA, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {message: ingredientsArray}
+        )
+    });
+    const recipe = await response.json()as RecipeInterface;
+    console.log(JSON.stringify({message: ingredientsArray}));
+    return recipe;
+});
+
+const fetchGoogleImages = createAsyncThunk('recipeData/fetchGoogleImages', async (searchTerm : string) => {
+    if (searchTerm === '') return;
+    const response = await fetch(ENDPOINT_GOOGLE_IMAGES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({searchTerm})
+    });
+    const imageArray = await response.json()as Image[];
+    return imageArray;
+})
+
+
+const recipeDataSlice = createSlice({
+    name: 'recipeData',
+    initialState,
+    reducers: {
+        nextImage: (state) => {
+            if (state.googleImagesData.length < 1) return;
+            state.googleImagesData.shift();
+        },
+        addToSavedRecipes: (state, action : PayloadAction < RecipeInterface >) => {
+            if (state.savedRecipes.find(item => item.id === action.payload.id) !== undefined) return;
+            state.savedRecipes.push(action.payload);
+        },
+        removeFromSavedRecipes: (state, action : PayloadAction < RecipeInterface >) => {
+            state.savedRecipes = state.savedRecipes.filter(item => item.id !== action.payload.id);
+        }
+    },
+    extraReducers: {
+        // fetchRecipeData
+        [fetchRecipeData.pending.toString()]: (state) => {
+            state.recipeDataStatus = RecipeDataStatuses.PENDING;
+        },
+        [fetchRecipeData.fulfilled.toString()]: (state, action) => {
+            state.recipeDataStatus = RecipeDataStatuses.FULFILLED;
+            const {data} : {
+                data: RecipeInterface
+            } = action.payload;
+            // action.payload.data will return null if chatgpt doesn't find an acording recipe
+            if (data === null) 
+                return;
+            
+
+            state.recipeData = data;
+            console.log('payload', data);
+        },
+        [fetchRecipeData.rejected.toString()]: (state) => {
+            state.recipeDataStatus = RecipeDataStatuses.REJECTED;
+        },
+        // //////////////////////
+        // fetchGoogleImages
+        [fetchGoogleImages.pending.toString()]: (state) => {
+            state.googleImagesStatus = GoogleImagesStatuses.PENDING;
+        },
+        [fetchGoogleImages.fulfilled.toString()]: (state, action) => {
+            state.googleImagesStatus = GoogleImagesStatuses.FULFILLED;
+            if (action.payload.data === null || action.payload.data === undefined) 
+                return;
+            
+
+            state.googleImagesData = action.payload.data as Image[];
+        },
+        [fetchGoogleImages.rejected.toString()]: (state) => {
+            state.googleImagesStatus = GoogleImagesStatuses.REJECTED;
+        }
+    }
+});
+
+export {
+    fetchRecipeData,
+    fetchGoogleImages
+};
+export const { nextImage, addToSavedRecipes, removeFromSavedRecipes } = recipeDataSlice.actions;
+export default recipeDataSlice.reducer;
