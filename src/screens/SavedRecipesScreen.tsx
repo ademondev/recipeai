3189cba5ad0a68be120e2936e10ce3b9cbb1967b
Raki <killer.vp.mic@gmail.com>
2023-03-01 14:10:16 -0300

    added styles

--- src/screens/SavedRecipesScreen.tsx
+++ src/screens/SavedRecipesScreen.tsx
@@ -1,20 +1,32 @@
-import { Container, Space } from '@mantine/core';
+import { Container, createStyles, Space } from '@mantine/core';
 import { useAppSelector } from '../app/hooks';
 import MainBottomMenu from '../components/MainBottomMenu';
 import SavedRecipeComponent from '../components/SavedRecipeComponent';
 import SearchBar from '../components/SearchBar';
 
+const useStyles = createStyles((theme) => ({
+    root: {
+        maxWidth: '60%',
+        [theme.fn.smallerThan('sm')]: {
+            maxWidth: '100%'
+        },
+    
+    }
+}));
+
 
 const SavedRecipesScreen: React.FunctionComponent = () => {
     const savedRecipes = useAppSelector(state => state.recipeData.savedRecipesStorage);
     const searchBarState = useAppSelector(state => state.searchBar.searchBarState);
+    const { classes } = useStyles();
     // recipe.recipeName must be lower case as the searchbar wouldn't be able to filter
     // recipes properly due to the recipes having uppercase letters, not matching 
     // unless you write the names perfectly.
     const filteredSavedRecipes = savedRecipes.filter(recipe => recipe.recipeName.toLowerCase().includes(searchBarState));
     return (<>
         <SearchBar />
-        <Container>
+        <Space h='md' />
+        <Container className={classes.root}>
             {filteredSavedRecipes.map(recipe => {
                 return (
                     <SavedRecipeComponent
