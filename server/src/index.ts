4d1b9b4a5c0108ec2d50a6de82557d2f5d29ce89
Raki <killer.vp.mic@gmail.com>
2023-03-01 21:32:53 -0300

    fixed cors?

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,6 +37,14 @@ function parseJsonByteArray(byteArray: Uint8Array): any {
     return JSON.parse(jsonString);
 }
 
+// Even using cors(), I still get a CORS error, so I'm using this middleware to set the headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 router.post('/completion', async (req, res) => {
     try {
         console.log('request body: ', req.body);
